e9e241e7addb22569c467549d2a5595f035136a6
lnalborczyk <ladislas.nalborczyk@gmail.com>
2022-10-13 09:03:04 +0200

    uploading slides for cours02

---
+++ slides/_utils/pdf_from_slides.R
@@ -0,0 +1,39 @@
+########################################################
+# Generating pdf from quarto (revealjs) slides
+####################################################
+
+library(renderthis)
+library(pagedown)
+library(stringr)
+
+# listing extant .html slides in the /html/ folder
+slides <- list.files(
+    path = "html", pattern = ".html",
+    recursive = TRUE, full.names = TRUE
+    )
+
+for (input in slides) { # for each course
+
+    # printing progress
+    print(paste0("Printing '", input, "'...") )
+    
+    # defining the output
+    course <- str_extract_all(string = input, pattern = "(?<=html/).+(?=.html)")[[1]]
+    output <- paste0("pdf/", course, ".pdf")
+    
+    # printing it using renderthis
+    renderthis::to_pdf(
+        # from = input,
+        from = paste0("_", course, "/", course, ".qmd"),
+        # to = paste0("pdf/", course, "_renderthis.pdf"),
+        complex_slides = FALSE
+        )
+    
+    # printing it using pagedown
+    pagedown::chrome_print(
+        input = input,
+        output = output,
+        format = "pdf"
+        )
+    
+}
